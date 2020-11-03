1c68cd8254ef6fb800e83690cd7c1211645c1386
Aryan R.H <aryanrh@outlook.com>
2020-11-03 16:19:19 +0530

    Update db.class.php

--- db.class.php
+++ db.class.php
@@ -43,7 +43,7 @@ return $this->set_name_arh;
 <link rel="stylesheet" type="text/css" href="https://aryan-rh-cdn.web.app/arh.about-panel.css">	
 </head>
 <body>
-	<h3>Ip finder using json and jquery built by Aryan R.H follow me to get more updates</h3>
+	<h3>Simple PHP class to fetch data from database built by Aryan R.H follow me to get more updates</h3>
 <?php
 $class_test=new get_db_data_arh();
 $class_test->send_data('git_repo_test','id','1','value');
